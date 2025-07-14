d08de5305f50d1b4dfe388099eae68d5a7313d2a
Tohid Rostami <117815548+TohidRostami@users.noreply.github.com>
2025-07-14 17:09:01 +0330

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -30,9 +30,3 @@ function toggleMenu() {
     menuOpen.value = !menuOpen.value
 }
 </script>
-
-git init
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/TohidRostami/Tohid_Rostami_CV.git
-git push -u origin main
